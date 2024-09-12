44c8230b60c7f23ce47ce8ddd91040345f5531ef
Parth Barhanpurkar <parth.barhanpurkar@analog.com>
2024-09-12 16:22:18 -0400

    Setting up {project folder} for the template

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,32 @@
+import os
+
+PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+
+def remove_file(filepath):
+    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+
+
+def remove_dir(path):
+    os.rmdir(os.path.join(PROJECT_DIRECTORY, path))
+
+if __name__ == "__main__":
+    if "{{ cookiecutter.use_jupyterlab }}" != "y":
+        remove_dir("notebooks")
+
+    if "{{ cookiecutter.use_precommit }}" != "y":
+        remove_file(".pre-commit-config.yaml")
+
+    if "{{ cookiecutter.use_precommit }}" == "y":
+        os.system('echo "Initializing Git for pre-commit... \n"')
+        os.system("git init --initial-branch=main")
+        os.system('echo "Installing pre-commit hooks... \n"')
+        os.system("pre-commit install")
+        os.system('echo "Running pre-commit hooks... \n"')
+
+
+    if "{{ cookiecutter.use_github_actions }}" != "y":
+        remove_dir(".github")
+
+    if "{{ cookiecutter.use_docker }}" != "y":
+        remove_file("Dockerfile")
+        remove_file("docker-compose.yml")
