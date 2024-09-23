96259199cea5727fe91a43998f233e27eeb49fca
Parth Barhanpurkar <parth.barhanpurkar@analog.com>
2024-09-23 14:42:31 -0400

    Adding main file for CLI

---
+++ {{cookiecutter.project_folder}}/{{cookiecutter.module_name}}/__main__.py
@@ -0,0 +1,12 @@
+"""
+Command line interface module for {{cookiecutter.module_name}}
+
+This file can be changed based on the requirements and structure of the project.
+"""
+
+import typer
+
+app = typer.Typer(help="{{cookiecutter.short_description}}")
+
+if __name__ == "__main":
+    app()
