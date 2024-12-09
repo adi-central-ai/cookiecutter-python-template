1e04145f55d5aa952bd99952930e78d4f0e5884a
Parth Barhanpurkar <parth.barhanpurkar@analog.com>
2024-12-09 11:49:58 -0500

    Add fix for not implemented tests and remove package-mode

--- {{cookiecutter.project_folder}}/tests/__init__.py
+++ {{cookiecutter.project_folder}}/tests/__init__.py
@@ -1,7 +1,7 @@
 """Version test for {{ cookiecutter.namespace + '-' + cookiecutter.module_name if cookiecutter.namespace else cookiecutter.module_name }}."""
+import pytest
 
-
-
+@pytest.mark.skip(reason="No version test yet.")
 def test_version() -> None:
     """Check that all the version tags are in sync."""
 
